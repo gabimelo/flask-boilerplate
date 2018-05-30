fc132b5897598c21ca1246ffa1642c80d9d5a393
Gabriela Melo <gabimelo10@gmail.com>
2018-05-30 19:31:12 -0300

    configured for it to work with nginx and uwsgi, but dont know how to get it to communicate with mongo

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,27 @@
-FROM python:3.4-alpine
+FROM python:3.5
 
-COPY . /app
-WORKDIR /app
+RUN apt-get update
+RUN apt-get install -y --no-install-recommends \
+        libatlas-base-dev gfortran nginx supervisor
 
-RUN pip install -r requirements.txt
+RUN pip3 install uwsgi
 
-CMD ["python", "app.py"]
+COPY ./requirements.txt /project/requirements.txt
+
+RUN pip3 install -r /project/requirements.txt
+
+RUN useradd --no-create-home nginx
+
+RUN rm /etc/nginx/sites-enabled/default
+RUN rm -r /root/.cache
+
+COPY nginx.conf /etc/nginx/
+COPY flask-site-nginx.conf /etc/nginx/conf.d/
+COPY uwsgi.ini /etc/uwsgi/
+COPY supervisord.conf /etc/
+
+COPY /app /project
+
+WORKDIR /project
+
+CMD ["/usr/bin/supervisord"]
--- app/server.py
+++ app/server.py
@@ -7,9 +7,11 @@ client = MongoClient('mongodb://db:27017/')
 # client = MongoClient('mongodb://localhost:27017/')
 db = client.test_database
 
+
 @app.route("/")
 def hello():
     return "Hello World!"
+
 
 # to check DB connection:
 @app.route("/users", methods=['GET', 'POST'])
@@ -24,8 +26,9 @@ def users_route():
             name = request.form["name"]
             user_id = db.users.insert_one({'name': name}).inserted_id
             return jsonify({"status": "success", "payload": str(user_id)})
-        except Exception as e:
+        except Exception:
             return jsonify({"status": "failed", "payload": "Please insert a name"})
+
 
 if __name__ == "__main__":
     # app.run(host="localhost", debug=True)
